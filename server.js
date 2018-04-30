89d3d56e125c20e5e6865face1085db662fe2a7e
Joe Shepherd <joeshepmedia@gmail.com>
2018-04-30 12:01:07 -0500

    API GET route

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 const express = require("express");
 const app = express();
+const movieApi = require("imdb-api");
 
 app.use(express.static(__dirname + "/client"));
+
+app.get("/movies", (req, res, next) => {
+  movieApi
+    .search(
+      { title: req.query.keyword },
+      { apiKey: require("./server/key").key }
+    )
+    .then(movieData => {
+      console.log(movieData);
+    });
+  res.end();
+});
 
 app.listen(3000, () => {
   console.log("server listening on port 3000");
